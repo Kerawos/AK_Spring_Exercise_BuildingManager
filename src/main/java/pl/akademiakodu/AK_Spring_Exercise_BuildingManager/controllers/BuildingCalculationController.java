ade93e03d6224ba398ec6e0bd4d0d520fc258dbd
Kerawos <Maresowa@gmail.com>
2017-11-24 16:56:39 +0100

    @add converter service

--- src/main/java/pl/akademiakodu/AK_Spring_Exercise_BuildingManager/controllers/BuildingCalculationController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_BuildingManager/controllers/BuildingCalculationController.java
@@ -3,6 +3,9 @@ package pl.akademiakodu.AK_Spring_Exercise_BuildingManager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.akademiakodu.AK_Spring_Exercise_BuildingManager.models.Building;
 import pl.akademiakodu.AK_Spring_Exercise_BuildingManager.models.BuildingManager;
 import pl.akademiakodu.AK_Spring_Exercise_BuildingManager.models.UserInput;
 
@@ -13,6 +16,13 @@ public class BuildingCalculationController {
 
     @GetMapping("/build") public String buildingGet(Model model){
         model.addAttribute("userInput", new UserInput());
+        return "build";
+    }
+
+    @PostMapping("/build") public String buildingPost(@ModelAttribute("userInput") UserInput userInput, Model model){
+        Building potentialBuilding = new Building();
+        potentialBuilding = buildingManager.totalCostCalculator(userInput.getWidth(), userInput.getLength(),
+                userInput.getFloors(), userInput.getUndergroundFloors());
         return "build";
     }
 
---
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_BuildingManager/models/services/Converter.java
@@ -0,0 +1,18 @@
+package pl.akademiakodu.AK_Spring_Exercise_BuildingManager.models.services;
+
+import java.math.BigInteger;
+
+public class Converter {
+
+    public int stringIntoInteger(String str){
+
+        return 0;
+    }
+
+    public BigInteger stringIntoBigInteger(String str){
+
+        return null;
+    }
+
+
+}
